b43913d26ce7f6acefb3f54d81881f439ed64e6a
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-11-14 21:35:18 +0700

    - Add Recycler view - Update glide , recyclerview

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+//import androidx.recyclerview.widget.recyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: Fragment = recyclerView()
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.replace(R.id.layout,recyclerView,"fragment_recyclerView")
+        transaction.addToBackStack("fragment_list_view")
+        transaction.commit()
     }
 }
